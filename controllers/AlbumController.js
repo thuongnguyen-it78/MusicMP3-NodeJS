d82222a070356ce2faa0dcddb3f4f53dc79c4406
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-01-16 15:08:20 +0700

    do album

---
+++ controllers/AlbumController.js
@@ -0,0 +1,25 @@
+const Album = require('../models/album.model')
+const Song = require('../models/song.model')
+
+class AlbumController {
+    async getOne(req, res, next) {
+        const id = req.params.id
+        const album = await Album.findById(id)
+        const length = album.listSongs.length
+        if(length === 0) return res.status(200).json({flag: true, data: []})
+        let listSongs = []
+
+        try {
+
+            listSongs = await Song.find({_id: { $in : album.listSongs} })
+            
+            res.status(200).json({flag: true, data: listSongs})
+        
+        } catch (error) {
+            res.status(500).json({flag: false, data: []})
+        }
+
+    }
+}
+
+module.exports = new AlbumController()
--- routes/album.route.js
+++ routes/album.route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const albumController = require('../controllers/AlbumController')
 
+router.get('/:id', albumController.getOne)
 
 
 
